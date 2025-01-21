c0b0b21cb9ec694f2960c18121c506fa4e62d88c
mezau0 <70231270+mezau0@users.noreply.github.com>
2025-01-21 16:16:21 -0500

    Font, basic files, css

---
+++ scriptgf.js
@@ -0,0 +1,80 @@
+// Open the Modal
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+  }
+  
+  // Close the Modal
+  function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+  }
+
+  //close modal by clicking esc key. arrowleft and arrowright 
+  //would be for the next & prev keys, but I can't figure it out
+  document.addEventListener('keydown', function(event) {
+    const key = event.key; // const {key} = event; in ES6+
+    if (key === "Escape") {
+        closeModal();
+    }
+});
+  
+  var slideIndex = 1;
+  showSlides(slideIndex);
+  
+  // Next/previous controls
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  // Thumbnail image controls
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+  }
+
+//REFRESH game from conceptober page
+function refreshPage() {
+  document.getElementById('MemoryGame').src = document.getElementById('MemoryGame').src
+}
+
+//SLIDES 2
+var slideIndex1 = 1;
+showDivs1(slideIndex1);
+
+function plusDivs1(n) {
+  showDivs1(slideIndex1 += n);
+}
+
+function currentDiv1(n) {
+  showDivs1(slideIndex1 = n);
+}
+
+function showDivs1(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides1");
+  var dots = document.getElementsByClassName("demo1");
+  if (n > x.length) {slideIndex1 = 1}    
+  if (n < 1) {slideIndex1 = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" w3-red", "");
+  }
+  x[slideIndex1-1].style.display = "block";  
+  dots[slideIndex1-1].className += " w3-red";
+}
